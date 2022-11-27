5e82ba00ed902fbd1836e759357b50960509720c
guilhermedom <guilhermedfs@gmail.com>
2022-11-26 21:38:12 -0300

    feat: add "Basic Select" solution

---
+++ employee_names.sql
@@ -0,0 +1,6 @@
+/* Problem: Write a query that prints a list of employee names (i.e.:
+the name attribute) from the Employee table in alphabetical order. */
+
+SELECT NAME
+FROM   employee
+ORDER  BY NAME;
