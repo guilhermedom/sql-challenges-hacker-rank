073db679a4fe54612b3de5e99b6a3988a2ed5e46
guilhermedom <guilhermedfs@gmail.com>
2022-11-26 21:38:36 -0300

    feat: add "Basic Select" solution

---
+++ weather_observation_station_11.sql
@@ -0,0 +1,7 @@
+/* Problem: Query the list of CITY names from STATION that either do not
+start with vowels or do not end with vowels. Your result cannot contain
+duplicates. */
+
+SELECT DISTINCT city
+FROM   station
+WHERE  city NOT RLIKE '^[aeiou].*[aeiou]$';
