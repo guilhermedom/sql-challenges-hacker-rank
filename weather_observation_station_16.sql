e0db70bc592697ed7fc9104c143eccb658cceeda
guilhermedom <guilhermedfs@gmail.com>
2022-11-30 17:39:20 -0300

    feat: add "Aggregation" solution

---
+++ weather_observation_station_16.sql
@@ -0,0 +1,6 @@
+/* Problem: Query the smallest Northern Latitude (LAT_N) from STATION
+that is greater than 38.7780. Round your answer to 4 decimal places. */
+
+SELECT Round(Min(lat_n), 4)
+FROM   station
+WHERE  lat_n > 38.7780;
