40fc7f01a606ea53f644302ff1373fe96fce039e
guilhermedom <guilhermedfs@gmail.com>
2022-11-30 17:39:05 -0300

    feat: add "Aggregation" solution

---
+++ weather_observation_station_14.sql
@@ -0,0 +1,7 @@
+/* Problem: Query the greatest value of the Northern Latitudes (LAT_N)
+from STATION that is less than 137.2345. Truncate your answer to 4
+decimal places. */
+
+SELECT Truncate(Max(lat_n), 4)
+FROM   station
+WHERE  lat_n < 137.2345;
