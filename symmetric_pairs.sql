7ba2f6816438f327f84ead8ff73111587dac439f
guilhermedom <guilhermedfs@gmail.com>
2022-12-12 22:00:26 -0300

    docs: add comments explaining solution

--- symmetric_pairs.sql
+++ symmetric_pairs.sql
@@ -7,6 +7,9 @@ X1 = Y2 and X2 = Y1.
 Write a query to output all such symmetric pairs in ascending order by
 the value of X. List the rows such that X1 ≤ Y1. */
 
+-- First subquery selects symmetric pairs that have x1 <> y1. A self
+-- join is used to compare every row with every other possible row on
+-- the same table.
 SELECT x1,
        y1
 FROM   ((SELECT F.x AS X1,
@@ -17,6 +20,13 @@ FROM   ((SELECT F.x AS X1,
                            AND F2.x = F.y
                            AND F.x <> F.y)
         UNION
+		-- Second subquery selects symmetric pairs that have x1 = y1.
+		-- Group by x1 and y1 as we want to count the number of times a
+		-- symmetric pair happen. We need to select only symmetric pairs
+		-- with x1 = y1 that have Count(*) > 1. That is because, when
+		-- x1 = y1, the row we have on table F will appear on F2 and be
+		-- counted as a symmetric pair, when it is actually just a
+		-- duplicate of itself.
         (SELECT F.x AS X1,
                 F.y AS Y1
          FROM   functions F
@@ -27,5 +37,6 @@ FROM   ((SELECT F.x AS X1,
          GROUP  BY F.x,
                    F.y
          HAVING Count(*) > 1)) AS FUNCT
+-- x1 must be smaller or equal to y1, as requested.
 WHERE  x1 <= y1
 ORDER  BY x1;
